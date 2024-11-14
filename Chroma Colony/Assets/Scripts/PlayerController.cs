2ece9f6c008651933b67542f033b4ae87c3a382f
Wbeimar Agudelo <andrey.agudelo12@gmail.com>
2024-11-14 00:22:00 -0500

    Fix

--- Chroma Colony/Assets/Scripts/PlayerController.cs
+++ Chroma Colony/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEditor.Animations;
 
 public class PlayerController : MonoBehaviour
 {
